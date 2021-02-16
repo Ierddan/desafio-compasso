afb01347fe2ffdfe6a2b61d95fa8c56f979a2cb6
Iêrddan Costa <Ierddan_c@yahoo.com.br>
2021-02-16 15:14:52 -0300

    Initial commit

---
+++ src/main/java/br/com/desafiocompasso/DesafioCompassoApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiocompasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCompassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCompassoApplication.class, args);
+	}
+
+}
